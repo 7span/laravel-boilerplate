c0db8c098e87f055e7f427605c17c05dc30a5820
Urvashi Thakar <urvashithakar143@gmail.com>
2020-08-05 19:43:35 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## About Laravel Boilerplate
+
+The laravel boilerplate is project that uses the `Laravel` (Version 7.3) as a backend framework.It provides basic structure for starting any new laravel project.
+It has implemented the basic authetication provided by laravel.
+
+
+## Laravel Packages
+
+- [Laravel Telescope](https://laravel.com/docs/7.x/telescope) - Monitor request.
+
+## Notes
+
+1. Please run "npm install && npm run dev" command to compile your assets.
+
+
---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    @if(auth()->check() && auth()->user()->is_developer === 1)
+                        <a href="{{ url('/telescope') }}">Open Telescope</a>
+                    @else
+                        {{ __('You are logged in!') }}
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
