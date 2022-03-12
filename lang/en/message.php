6081bb56f56be3fe0ee22e80a6387956fccbc90d
Parth Gajjar <parth@7span.com>
2022-03-12 18:05:35 +0530

    Update message.php from lang folder

--- lang/en/message.php
+++ lang/en/message.php
@@ -5,5 +5,6 @@ return [
     'emailNotExist' => 'This email address is not registered in the system.',
     'forgetPasswordEmailSuccess' => 'Forgot Password email has been sent successfully.',
     'passwordChangeSuccess' => 'Password changed successfully.',
-    'invalidOtp' => 'Invalid OTP. Please try again.'
+    'invalidOtp' => 'Invalid OTP. Please try again.',
+    'invalidUrl' => 'The resource you are looking for could not be found.',
 ];
