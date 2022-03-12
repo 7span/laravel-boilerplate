3a5475c16f6e36699e3c27c9256a8b3a04eb17ad
Parth Gajjar <parth@7span.com>
2022-03-12 16:16:40 +0530

    Improve code as per latest structure and concepts

--- resources/views/emails/forget-password.blade.php
+++ resources/views/emails/forget-password.blade.php
@@ -1,10 +1,14 @@
 @component('mail::message')
-{{ __('email.hello') }} {{ $data['name'] }},
 
-{{ __('email.forgetPasswordEmailLine1',['otp' => $data['otp']]) }}
-<br>
-<br>
-{{ __('email.forgetPasswordEmailLine2') }}
+{{ __('email.hello') }} {{ $data['name'] }},<br>
+
+{{ __('email.forgetPasswordEmailLine1') }}
+
+<div class="otp">{{$data['otp']}}</div>
+
+{{ __('email.forgetPasswordEmailLine2') }}<br>
 
 {{ __('email.thanks') }},<br>
+{{ config('app.name') }}
+
 @endcomponent
