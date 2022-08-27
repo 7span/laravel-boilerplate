f03ebf3ee4a0b3857e8a2b6ee69087ea0344f19d
Parth Gajjar <parth@7span.com>
2022-08-27 14:06:53 +0530

    added default user via seeder

---
+++ config/user-seeder.php
@@ -0,0 +1,13 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Hash;
+
+return [
+    [
+        'name' => 'Admin',
+        'email' => 'admin@example.com',
+        'email_verified_at' => Carbon::now(),
+        'password' => 'password'
+    ]
+];
---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = config('user-seeder');
+
+        if (!empty($users)) {
+            foreach (collect($users) as $user) {
+                User::create([
+                    'name' => $user['name'],
+                    'email' => $user['email'],
+                    'email_verified_at' => $user['email_verified_at'],
+                    'password' => $user['password'],
+                ]);
+            }
+        }
+    }
+}
