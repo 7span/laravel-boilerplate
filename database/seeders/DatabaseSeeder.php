a5356ed99b68a6a0cc93053b3e3de1b4d95b34f2
dhrumin-7span <dhrumin@7span.com>
2024-04-08 10:25:31 +0530

    Changes in factory and seeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'first_name' => 'Test',
+            'last_name' => 'User',
             'email' => 'test@example.com',
         ]);
     }
