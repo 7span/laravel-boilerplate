8180e10b2eb4138a5e6ca13047ba5c13d3d9eb08
Trushal Thummarom <trushal@7span.com>
2024-05-20 12:56:20 +0530

    Remove unwanted changes

--- database/migrations/0001_01_01_000001_create_cache_table.php
+++ database/migrations/0001_01_01_000001_create_cache_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
         Schema::create('cache', function (Blueprint $table) {
             $table->string('key')->primary();
             $table->mediumText('value');
-
             $table->integer('expiration');
         });
 
         Schema::create('cache_locks', function (Blueprint $table) {
             $table->string('key')->primary();
             $table->string('owner');
-
             $table->integer('expiration');
         });
     }
