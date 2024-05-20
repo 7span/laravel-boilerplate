47838b634440d1f3913132cef07ae00ef808124d
Trushal Thummarom <trushal@7span.com>
2024-05-20 12:29:48 +0530

    set duster & husky

--- database/migrations/0001_01_01_000001_create_cache_table.php
+++ database/migrations/0001_01_01_000001_create_cache_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
         Schema::create('cache', function (Blueprint $table) {
             $table->string('key')->primary();
             $table->mediumText('value');
+
             $table->integer('expiration');
         });
 
