1c2057b90d13f70ba64844ec7eb09f068211dd5f
Trushal Thummar <102795093+trushal-7span@users.noreply.github.com>
2024-05-22 15:52:16 +0530

    Update ExceptionHelper.php

--- app/Helpers/ExceptionHelper.php
+++ app/Helpers/ExceptionHelper.php
@@ -2,22 +2,25 @@
 
 namespace App\Helpers;
 
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use App\Exceptions\CustomException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class ExceptionHelper
 {
-    public static function notFoundHandler(NotFoundHttpException $e, Request $request): ?JsonResponse
+    public static function notFoundHandler(NotFoundHttpException $e, Request $request)
     {
-        if ($request->is('api/*')) {
-            $modelName = last(explode('\\', $e->getPrevious()->getModel()));
-            $modelKey = strtolower(preg_replace('/([a-z])([A-Z])/s', '$1_$2', $modelName));
+        if ($request->is('api/*') && $e->getPrevious() instanceof ModelNotFoundException) {
+            if (method_exists($e->getPrevious(), 'getModel')) {
+                $modelName = last(explode('\\', $e->getPrevious()->getModel()));
+            } else {
+                $modelName = 'Resource';
+            }
             $modelName = preg_replace('/([a-z])([A-Z])/s', '$1 $2', $modelName);
             $modelName = strtolower($modelName);
             $modelName = ucfirst($modelName);
-            $error['errors'][$modelKey][] = __('entity.entityNotFound', ['entity' => "$modelName data"]);
-            return response()->json($error, 404);
+            throw new CustomException(__('entity.entityNotFound', ['entity' => "$modelName data"]));
         }
     }
 }
