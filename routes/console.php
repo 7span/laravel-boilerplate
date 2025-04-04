1a9b3a43a829864d1a5ea1e110f1fcff5094da47
dhrumin-7span <dhrumin@7span.com>
2025-04-04 19:13:31 +0530

    Remove daily schedule for clearing old logs command

--- routes/console.php
+++ routes/console.php
@@ -6,4 +6,3 @@ use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('telescope:prune')->daily();
 Schedule::command('pulse:purge')->daily();
-Schedule::command('app:clear-old-logs')->daily();
