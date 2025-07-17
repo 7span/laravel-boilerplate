6374156219497d17b182f5b47aecb22f7668e54e
Harshil Patanvadiya <harshil@7span.com>
2025-07-17 17:14:40 +0530

    Update environment configuration and implement API rate limiting. Change cache store to Redis and switch Redis client to Predis. Apply throttle middleware to API routes for improved request handling.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Support\Facades\RateLimiter;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)
+                ->by($request->user()?->id ?: $request->ip())
+                ->response(function (Request $request, array $headers) {
+                    return response()->json([
+                        'message' => 'You have exceeded the maximum number of allowed requests. Please try again later.',
+                    ], 429, $headers);
+                });
+        });
+
         if ($this->app->runningInConsole()) {
             $this->commands([]);
         }
