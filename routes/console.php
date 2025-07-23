44f22393fc7db797b406eff719f0e1f8c76085e9
dhrumin-7span <dhrumin@7span.com>
2025-07-23 18:30:26 +0530

    Added command in scheduling with comment.

--- routes/console.php
+++ routes/console.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Schedule;
 Schedule::command('media:delete-temp-files')->daily();
 Schedule::command('telescope:prune --hours=24')->daily();
 Schedule::command('pulse:purge')->daily();
+
+// To permanently delete soft-deleted records after X days
+// Schedule::command('system:hard-delete-data')->daily();
