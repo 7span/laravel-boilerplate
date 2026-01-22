01c7fb1c0ec163a227faac3a82af9cc879f7083f
dhrumin-7span <dhrumin@7span.com>
2026-01-22 11:13:04 +0530

    Optimize update method in SettingService to batch retrieve settings
    - Updated the update logic to fetch all relevant settings in a single query using `whereIn`, improving performance and reducing database calls during updates.

--- app/Services/SettingService.php
+++ app/Services/SettingService.php
@@ -30,9 +30,13 @@ class SettingService
 
     public function update(array $inputs): array
     {
+        $settings = $this->settingObj->getQB()
+            ->whereIn('key', array_keys($inputs))
+            ->get()
+            ->keyBy('key');
+
         foreach ($inputs as $key => $value) {
-            $setting = $this->settingObj->where('key', $key)->firstOrFail();
-            $setting->update(['value' => $value]);
+            $settings[$key]->update(['value' => $value]);
         }
 
         $data['message'] = __('entity.entityUpdated', ['entity' => 'Master Setting']);
