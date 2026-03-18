543e1122463fe069f51d90754f43970316e30f43
sanjay <sanjay@7span.com>
2026-03-18 15:43:39 +0530

    laravel version update

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -4,28 +4,21 @@ namespace App\Models;
 
 use App\Traits\BaseModel;
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\HasModelAttributes;
+use App\Attributes\Fillable;
+use App\Attributes\Hidden;
 
+#[Fillable(['id', 'user_id', 'sent_by', 'title', 'description', 'type', 'notifiable_type', 'notifiable_id', 'data', 'read_at'])]
+#[Hidden(['id'])]
 class Notification extends Model
 {
-    use BaseModel;
+    use BaseModel, HasModelAttributes;
 
     public $incrementing = false;
 
     protected $keyType = 'string';
 
-    protected $fillable = [
-        'id',
-        'user_id',
-        'sent_by',
-        'title',
-        'description',
-        'type',
-        'notifiable_type',
-        'notifiable_id',
-        'data',
-        'read_at',
-    ];
-
+    
     protected $casts = [
         'data' => 'array',
         'read_at' => 'timestamp',
@@ -56,4 +49,10 @@ class Notification extends Model
     {
         return $this->belongsTo(User::class, 'sent_by');
     }
+
+    public function scopeIsRead($query)
+    {
+        return $query->whereNotNull('read_at');
+    }
+
 }
