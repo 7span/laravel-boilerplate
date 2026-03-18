fc203571fd77d4078b88f335f4edcb5302f3a6a3
sanjay <sanjay@7span.com>
2026-03-18 15:43:39 +0530

    laravel version update

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -4,21 +4,15 @@ namespace App\Models;
 
 use App\Traits\BaseModel;
 use Illuminate\Database\Eloquent\Model;
+use App\Attributes\Fillable;
+use App\Attributes\Hidden;
+use App\Traits\HasModelAttributes;
 
+#[Fillable(['id', 'name', 'iso', 'iso3', 'calling_code', 'currency', 'icon', 'status'])]
+#[Hidden(['id'])]
 class Country extends Model
 {
-    use BaseModel;
-
-    protected $fillable = [
-        'name',
-        'iso',
-        'iso3',
-        'iso_code',
-        'calling_code',
-        'currency',
-        'icon',
-        'status',
-    ];
+    use BaseModel, HasModelAttributes;
 
     protected $defaultSort = 'name';
 
