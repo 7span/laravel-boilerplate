ff85962fe78fc75ec1d52d3c04d62c8cfbc036c9
dhrumin-7span <dhrumin@7span.com>
2026-03-19 15:32:10 +0530

    Add strict types declaration across the application and introduce Laravel Pint configuration

--- app/Http/Controllers/Developer/DeveloperController.php
+++ app/Http/Controllers/Developer/DeveloperController.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace App\Http\Controllers\Developer;
 
 use App\Http\Controllers\Controller;
--- app/Http/Resources/Country/Resource.php
+++ app/Http/Resources/Country/Resource.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace App\Http\Resources\Country;
 
 use App\Models\Country;
