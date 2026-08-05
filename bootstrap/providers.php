1178da14b4f1ce50a0da21257a120c9cb78ff909
dhrumin-7span <dhrumin@7span.com>
2026-08-05 19:29:29 +0530

    Update dependencies in composer.json, enhance PHPStan level, and configure routing for API. Added Horizon and Telescope service providers.

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Providers\AppServiceProvider;
+use App\Providers\HorizonServiceProvider;
+use App\Providers\TelescopeServiceProvider;
 
 return [
     AppServiceProvider::class,
+    HorizonServiceProvider::class,
+    TelescopeServiceProvider::class,
 ];
